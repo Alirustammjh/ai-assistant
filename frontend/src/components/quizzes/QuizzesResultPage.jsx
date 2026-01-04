28d680793090c5b7afb6257cfa5f039b482a4f72
Alirustammjh <alirustammjh@gmail.com>
2026-01-04 19:18:39 +0530

    Frontend setup

---
+++ frontend/src/components/quizzes/QuizzesResultPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QuizzesResultPage = () => {
+  return (
+    <div>QuizzesResultPage</div>
+  )
+}
+
+export default QuizzesResultPage
---
+++ frontend/src/components/quizzes/QuizzesTakePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QuizzesTakePage = () => {
+  return (
+    <div>QuizzesTakePage</div>
+  )
+}
+
+export default QuizzesTakePage
